dcfec84fdd332b73fd651691e768b2809086ab8d
wakoIniad <s1f102400606@iniad.org>
2026-02-14 15:17:34 +0900

    make displayer

--- Assets/Scripts/ValueSetter.cs
+++ Assets/Scripts/ValueSetter.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 using System.Text.RegularExpressions;
 using TMPro;
 
-public class EndingSceneDisplayer : MonoBehaviour
+public class ValueSetter : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void SetValue(string[] values) {
+    public void SetValue(params string[] values) {
         int count = 0;
         string placeholder = textMeshPro.text;
         string replaced = Regex.Replace(placeholder, "%N", m =>
